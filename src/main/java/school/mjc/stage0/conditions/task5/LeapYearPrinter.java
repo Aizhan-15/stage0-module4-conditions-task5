63093d6861e8ede0441581f62844d86c2e75d58f
Aizhan-15 <aizhanurman@gmail.com>
2022-10-12 22:15:03 +0600

    completed all the tasks

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        int year1 = 2020;
+        if (year1 % 400 == 0) {
+            System.out.println("leap");
+        } else {
+            System.out.println("not leap");
+        }
     }
 }
